ce01b9af330fdc6b3224483c6e1f1915cb71fd35
Vincent McKen <mcken.vince@gmail.com>
2021-07-20 20:56:05 +0000

    completed flatten

---
+++ flatten.js
@@ -0,0 +1,17 @@
+const flatten = (array) => {
+  const flattenedArray = [];
+
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      for (let subElement of element) {
+        flattenedArray.push(subElement);
+      }
+    } else {
+      flattenedArray.push(element);
+    }
+  }
+  return flattenedArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+console.log(flatten([1, 5, 2, 3, [7, 8, 9, [1, 2, 3], 6], [9, 8, 4], 2]));
