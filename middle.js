1b95888829b692210bf480a5cd981956af03ba5e
Vincent McKen <mcken.vince@gmail.com>
2021-07-21 00:00:20 +0000

    complete middle function

---
+++ middle.js
@@ -0,0 +1,28 @@
+const middle = (array) => {
+  let mid;
+  
+  const isEven = (array) => {
+    if (array.length % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  if (array.length > 2) {
+    if (isEven(array)) {
+      mid = [array.length / 2 - 1, array.length / 2];
+      return [array[mid[0]], array[mid[1]]];
+    } else {
+      mid = Math.floor(array.length / 2);
+      return [array[mid]];
+    }
+  } else {
+    return [];
+  }
+};
+
+console.log(middle([1,2,3,4,5]));
+console.log(middle(["one", "two", "three", "four"]));
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
