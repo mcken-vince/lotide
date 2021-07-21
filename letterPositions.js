4b149f1adef02baf104a4f46f9e7cf4f99271830
Vincent McKen <mcken.vince@gmail.com>
2021-07-21 15:32:59 +0000

    completed letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,15 @@
+const letterPositions = (string) => {
+  const results = {};
+
+  for (let l = 0; l < string.length; l++) {
+    if (string[l] !== " ") {
+      if (results[string[l]]) {
+        results[string[l]].push(l);
+      } else {
+        results[string[l]] = [l];
+      }
+    }
+  }
+
+  return results;
+};
