86827925e952ca6c15040508ec4c162e71828417
Vincent McKen <mcken.vince@gmail.com>
2021-07-21 15:11:23 +0000

    completed countLetters function

---
+++ countLetters.js
@@ -0,0 +1,17 @@
+const countLetters = (string) => {
+  const letterCount = {};
+  const noSpaces = string.split(" ").join("");
+  console.log(noSpaces);
+  for (let letter of noSpaces) {
+    if (letterCount[letter]) {
+      letterCount[letter] += 1;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+};
+
+console.log(countLetters("hello"));
+console.log(countLetters("hi my name is Vincent"));
+console.log(countLetters("supercalifragilisticexpialidocious"));
