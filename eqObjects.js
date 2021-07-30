aabfb307f9695fd391a19aed47ef307d98375b68
Vincent McKen <mcken.vince@gmail.com>
2021-07-30 03:02:07 +0000

    updated eqObjects to use recursion, works for nested objects/arrays

--- eqObjects.js
+++ eqObjects.js
@@ -1,18 +1,37 @@
-const eqObjects = (obj1, obj2) => {
-  if (Object.keys(obj1).length === Object.keys(obj2).length) {
-    for (let key in obj1) {
-      if (Array.isArray(obj1[key])) {
-        if (!eqArrays(obj1[key], obj2[key])) {
-          return false;
-        }
-      } else if (obj2[key] !== obj1[key]) {
-        return false;
-      }
-    }
-  } else {
+const eqArrays = require('./eqArrays');
+
+const eqObjects = (o1, o2) => {
+  if (Object.keys(o1).length !== Object.keys(o2).length) {
+    console.log(`Object.keys(o1).length !== Object.keys(o2)`);
     return false;
+  }
+  for (let key in o1) {
+    // handles if element is an object
+    if (typeof o1[key] === 'object') {
+      console.log(`Object.isObject(o1[key])`)
+      return eqObjects(o1[key], o2[key]);
+    }
+    // handles if element is an array
+    if (Array.isArray(o1[key])) {
+      console.log(`Array.isArray(o1[key])`)
+      return eqArrays(o1[key], o2[key]);
+    }
+    // if values are not equal
+    if (o1[key] !== o2[key]) {
+      console.log(`o1[key] !== o2[key] ||| ${o1[key], o2[key]} `)
+      return false;
+    }
   }
   return true;
 };
 
+
 module.exports = eqObjects;
+
+
+// console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+
+// console.log(eqObjects({a: {b: {c: {z: 5}, d: {e: 10}}, f: 8, g: 9}, h: 7}, {a: {b: {c: {z: 5}, d: {e: 10}}, f: 8, g: 9}, h: 7})); // => true
